360a432b99bdcbc826f483f9205ef5d89e935190
Kiss Tibor <kisss.tibi@gmail.com>
2025-05-16 10:53:38 +0200

    profil icon

--- accounts/views/home.py
+++ accounts/views/home.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from accounts.models import Profile  # Ha a Profile model itt van
 
 def home(request):
-    return render(request, 'home.html')
+    profile = None
+    if request.user.is_authenticated:
+        profile = Profile.objects.filter(user=request.user).first()  # Betöltjük a bejelentkezett felhasználó profilját
+    return render(request, 'home.html', {'profile': profile})
