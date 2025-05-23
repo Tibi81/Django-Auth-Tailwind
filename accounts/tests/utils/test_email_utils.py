b1406495d6aa3a814078696f93bde8c73b5e0b1a
Kiss Tibor <kisss.tibi@gmail.com>
2025-05-23 19:29:03 +0200

    test_email_utils.py+

--- accounts/tests/utils/test_email_utils.py
+++ accounts/tests/utils/test_email_utils.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 from accounts.models import Profile
 from django.utils.timezone import now
 from datetime import timedelta
-from django.core.mail import outbox
 from accounts.utils.token_utils import generate_email_token
 from accounts.utils.email_utils import send_verification_email
+from django.urls import reverse
 
 @override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')
 class EmailVerificationTestCase(TestCase):
@@ -46,3 +46,20 @@ class EmailVerificationTestCase(TestCase):
     def test_verify_email_invalid_token(self):
         response = self.client.get("/verify-email/invalid_token/")
         self.assertEqual(response.status_code, 302)  # Átirányítás a bejelentkezéshez
+
+
+    def test_verify_email_expired_token(self):
+        token = generate_email_token()
+        self.profile.email_token = token
+        self.profile.email_token_expires = now() - timedelta(hours=1)  # Token lejárt
+        self.profile.save()
+
+        response = self.client.get(reverse('verify_email', kwargs={'token': token}))
+
+        # Ellenőrizzük, hogy az e-mail továbbra sem megerősített
+        self.profile.refresh_from_db()
+        self.assertFalse(self.profile.email_verified)
+        self.assertIsNotNone(self.profile.email_token)  # A token megmarad, mert lejárt
+
+        # Ellenőrizzük, hogy a felhasználó megfelelő üzenetet kapott
+        self.assertEqual(response.status_code, 302)  # Átirányítás egy másik oldalra (például új token kérésére)
